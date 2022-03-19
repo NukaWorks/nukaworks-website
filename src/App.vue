c302ec89818a1cf66f1318f97030375af5df6139
Powerm1nt <sundev79@nutdevs.org>
2022-03-19 23:45:38 +0100

    HelloWorld: Updated text

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <HelloWorld msg="Hello World !" />
+  <HelloWorld msg="NukaWorks." />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from "./components/HelloWorld/HelloWorld.vue";
 
 export default {
   name: "App",
@@ -14,14 +14,14 @@ export default {
 </script>
 
 <style lang="scss">
-html,
-body {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
+html, body 
+  {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
 #app {
-  background: #1a212b;
+background: #1a212b;
 }
 </style>
--- src/components/HelloWorld/HelloWorld.vue
+++ src/components/HelloWorld/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h3>Build a powerful ecosystem. Soon.</h3>
   </div>
 </template>
 
@@ -19,15 +20,20 @@ export default {
 .hello {
   min-height: 100vh;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 
-  &,
-  h1 {
+   & h1, h3 {
+    margin: 0;
     font-size: calc(1rem + 5vw);
     font-family: "Outfit", sans-serif;
     color: white;
     text-shadow: 0 0 5px white;
   }
+
+   & h3 {
+    font-size: 15pt;
+   }
 }
 </style>
